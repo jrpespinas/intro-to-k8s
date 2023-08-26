cb595f04bd5467fcdac71926516c528b742d481b
jrpespinas <jrpespinas@gmail.com>
2023-08-27 01:13:23 +0800

    chore: implement redis deployment and service

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from redis import Redis
 from fastapi import FastAPI
 
 app = FastAPI()
+redis_client = Redis(host='redis-service', port=6379)
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello, world!"}
+async def index():
+    redis_client.incr('visitor')
+    return {"message": f"Hello, visitor #{redis_client.get('visitor')}!"}
